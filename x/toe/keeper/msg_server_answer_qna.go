a67777b970e5f931cf64d283c831f263d7c3f056
WilliamKelly00 <williammkelly00@gmail.com>
2022-04-13 13:30:24 -0400

    transfer ownership of question on correct answer

--- x/toe/keeper/msg_server_answer_qna.go
+++ x/toe/keeper/msg_server_answer_qna.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/WilliamKelly00/toev2/x/toe/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) AnswerQna(goCtx context.Context, msg *types.MsgAnswerQna) (*types.MsgAnswerQnaResponse, error) {
@@ -18,8 +19,33 @@ func (k msgServer) AnswerQna(goCtx context.Context, msg *types.MsgAnswerQna) (*t
 	// else
 		// pay coins (do later)
 
-		
-	_ = ctx
+
+	// Try getting a qna from the store
+	qna, isFound := k.GetQna(ctx, msg.Qsh)
+	
+	if !isFound{
+		// wrong answer
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Wrong answer")
+	}
+
+	// payments
+
+	// Transfer ownership
+
+	newQna := types.Qna{
+		Index: qna.Qsh, 
+		Qsh: qna.Qsh, 
+		ParentTopic: qna.ParentTopic, 
+		Question: qna.Question, 
+		OpA: qna.OpA, 
+		OpB: qna.OpB, 
+		OpC: qna.OpC, 
+		OpD: qna.OpD, 
+		Reward: qna.Reward, 
+		Owner: msg.Creator, 
+	}
+
+	k.SetQna(ctx, newQna)
 
 	return &types.MsgAnswerQnaResponse{}, nil
 }
